9d9f5127cf690806c954088cec47cd653581a1bd
Denis Den <inspostmail123@gmail.com>
2021-01-21 20:18:56 +0300

    1st module tasks

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,19 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+
+	let result;
+
+	if (n === 0) {
+		return 1;
+	} else if (n > 0) {
+
+		for (var i = result = 1; i <= n; i++) {
+			result *= i;
+		}
+		return result;
+
+	} else {
+		return false;
+	}
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,14 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+	
+	let firstSpamWord = '1xBet';
+	let secSpamWord = 'XXX';
+
+	str = str.toLowerCase();
+	firstSpamWord = firstSpamWord.toLowerCase();
+	secSpamWord = secSpamWord.toLowerCase();
+	
+	if (str.includes(firstSpamWord) || str.includes(secSpamWord)) return true;
+	return false;
 }
